64db31972f75895fa3b259eedb44fb31f93af17d
masic0729 <pihlan0620@gmail.com>
2025-04-14 11:45:35 +0900

    wave work02
    ㅈㄱㄴ

--- Prototype/Assets/Scripts/Managers/SpawnData.cs
+++ Prototype/Assets/Scripts/Managers/SpawnData.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SpawnData : MonoBehaviour
 {
-    //public GameObject[] spawnEnemy;
 
     [System.Serializable]
     public class SpawnInfomation
--- Prototype/Assets/Scripts/Managers/SpawnManager.cs
+++ Prototype/Assets/Scripts/Managers/SpawnManager.cs
@@ -1,16 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class SpawnManager : MonoBehaviour
 {
-    public static SpawnManager instance;
-    SpawnData spawnData;
-    private Coroutine spawnCoroutine;
+    public static SpawnManager instance;            //싱글톤. 사실 의미 없을 수도 있지만, 참조를 의도로 기능 활성화
+    SpawnData spawnData;                            //적 스폰에 대한 데이터
+    float waveTimer = 0, waveTime = 99f;            //기본적인 스폰 관련 타이머
+    int waveIndex;
+    bool isWaveTimerOn;                             //외부 객체에 의해 웨이브 타이머가 통제될 수 있음
+    bool isSpawning;                                //몬스터 스폰 중에는 스폰 타이머가 돌아가지 않음
+
     private void Awake()
     {
+        //싱글톤
         if(instance == null)
         {
             instance = this;
@@ -23,39 +25,85 @@ public class SpawnManager : MonoBehaviour
 
     private void Start()
     {
+        Init();
+    }
+
+    private void Update()
+    {
+        WaveTimer();
+    }
+
+    void Init()
+    {
         spawnData = this.gameObject.GetComponent<SpawnData>();
-        spawnCoroutine = StartCoroutine(WaveCoroutine());
-        Invoke("Test", 1.0f);
+        SetWaveTimerOnState(false);
+        SetIsSpawningState(false);
+        waveIndex = 0;
     }
 
-    private void LateUpdate()
+    void WaveDataLoad(SpawnData data)
     {
-
+        //waveTime = 
     }
 
-    void Test()
+    void WaveTimer()
     {
-        //StopCoroutine(spawnCoroutine);
-
-    }
-
-    void Test2()
-    {
-        //StartCoroutine(spawnCoroutine);
-    }
-
-    IEnumerator WaveCoroutine()
-    {
-        Repeat:
-        Debug.Log("안녕");
-
-        /*for(int i = 0; i < spawnData.spawnDataList.Count; i++)
+        if (GetWaveTimerOnState() == true && GetIsSpawningState() == false)
         {
-            yield return new WaitForSeconds(0);
+            waveTimer += Time.deltaTime;
+        }
+        if (waveTimer > waveTime)
+        {
+            SetTimer(0);
+            WaveOn();
+        }
+    }
 
-        }*/
-        yield return new WaitForSeconds(0);
+    
 
-        goto Repeat;
+    void WaveOn()
+    {
+        StartCoroutine(SpawnCoroutine(spawnData));
+    }
+
+    IEnumerator SpawnCoroutine(SpawnData spawnInfo)
+    {
+        SetIsSpawningState(true);
+
+
+        for (int i = 0; i < spawnData.spawnDataList.Count; i++)
+        {
+
+            yield return new WaitForSeconds(spawnInfo.spawnDataList[waveIndex].spawnDelay);
+
+        }
+
+        waveIndex++;
+        SetIsSpawningState(false);
+    }
+
+    void SetTimer(float value)
+    {
+        waveTimer = value;
+    }
+
+    public void SetWaveTimerOnState(bool state)
+    {
+        isWaveTimerOn = state;
+    }
+
+    public bool GetWaveTimerOnState()
+    {
+        return isWaveTimerOn;
+    }
+
+    void SetIsSpawningState(bool state)
+    {
+        isSpawning = state;
+    }
+
+    bool GetIsSpawningState()
+    {
+        return isSpawning;
     }
 }
