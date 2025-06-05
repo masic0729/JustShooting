98faa2bb2086301b53bc5ca65f26dff4d0f300f6
masic0729 <pihlan0620@gmail.com>
2025-06-05 12:20:41 +0900

    cardLoad

--- Prototype/Assets/Scripts/Card/CardInfo.cs
+++ Prototype/Assets/Scripts/Card/CardInfo.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "Card Data", menuName = "Scriptable Object/Card Data")]
 public class CardInfo : ScriptableObject
 {
     // cardData 변수 선언: 카드의 상세 데이터가 저장됨
-    public PlayerCardData cardData;
+    //public PlayerCardData cardData;
+    /// <summary>
+    /// 카드의 고유 타입 정의 (무기 강화, 스킬 강화)
+    /// </summary>
+    public enum CardType
+    {
+        Weapon,     // 플레이어 무기 강화
+        Skill       // 플레이어 기술 부여 및 강화
+    }
 
+    // 카드 프레임 이미지
+    public Sprite background;
+    // 카드 아이콘 이미지
+    public Sprite icon;
+    // 카드 이름
+    public string cardName;
+    // 카드 설명 텍스트
+    public string description;
+    // 현재 카드 레벨
+    public int currentLevel;
+    // 카드 최대 레벨
+    public int maxLevel;
+    // 스킬 카드 여부 (true면 스킬 카드)
+    public bool isSkill;
     /// <summary>
     /// 카드가 발동할 때 실행되는 기능
     /// </summary>
--- Prototype/Assets/Scripts/Card/CardManager.cs
+++ Prototype/Assets/Scripts/Card/CardManager.cs
@@ -1,18 +1,82 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CardManager : MonoBehaviour
 {
+    //리스트 관련해서 공부하는 기회의 스크립트. 결국 이 스크립트로 최종적으로 구현할 예정이지만, 학습에 중점을 두고 있음
     [SerializeField]
     //현재 보여지는 카드 게임오브젝트 배열
-    private GameObject[] currentCards;
+    GameObject[] currentCards;
     CardInfo[] loadedCards;
+
     //전체 카드 정보 목록
     public List<CardInfo> allCards;
 
-    /*
+    private void Start()
+    {
+        CardDataLoad();
+        Test();
+        ShowCards();
+    }
+
+    private void Update()
+    {
+        ShowCards();
+
+    }
+
+    //리소스 폴더에서 모든 카드 데이터를 로드하여 리스트에 저장
+    void CardDataLoad()
+    {
+        loadedCards = Resources.LoadAll<CardInfo>("Scriptable/Card/");
+        allCards = loadedCards.ToList();
+    }
+
+    /// <summary>
+    /// 기본적으로 3장의 카드를 무작위로 가져온다.
+    /// 획득 제한 및 카드가 부족한 경우 3장 미만으로 반환할 수 있음.
+    /// </summary>
+    /// <param name="getCount">가져올 카드 개수</param>
+    /// <returns>선택된 카드 리스트</returns>
+    public List<CardInfo> GetCards(int getCount = 3)
+    {
+        List<CardInfo> dupliCards = allCards;
+
+        List<CardInfo> randCards = allCards.OrderBy(_ => Random.value).ToList();
+
+        return randCards.Take(getCount).ToList();
+    }
+
+    void SetCard(GameObject card, CardInfo info)
+    {
+        Debug.Log(card.transform.Find("CardFrame/Icon"));
+        card.transform.Find("CardFrame/Icon").GetComponent<Image>().sprite = info.icon;
+
+    }
+
+    /// <summary>
+    /// 현재는 기본 구현으로 만들고, 이후 기획안을 기반으로 구현할 것.
+    /// </summary>
+    public void ShowCards()
+    {
+        List<CardInfo> instanceCards = GetCards();
+        Debug.Log(instanceCards[0].icon);
+        for(int i = 0; i < GetCards().Count; i++)
+        {
+            SetCard(currentCards[i], instanceCards[i]);
+        }
+
+    }
+
+    void Test()
+    {
+        //for(int i = 0; i < )
+    }
+
+    void tresh()
+    {
+        /*
     //특정 개수만큼 등장 가능한 카드 리스트를 필터링하여 반환 (주석 처리됨)
     public List<PlayerCardData> GetAvailableCardChoices(int count)
     {
@@ -58,34 +122,6 @@ public class CardManager : MonoBehaviour
         return selectedSkill.Concat(selectedNormal).ToList();
     }
     */
-
-    //게임 오브젝트가 활성화될 때 초기화 호출
-    private void Awake()
-    {
-        CardInit();
     }
 
-    //리소스 폴더에서 모든 카드 데이터를 로드하여 리스트에 저장
-    void CardInit()
-    {
-        loadedCards = Resources.LoadAll<CardInfo>("Scriptable/Card/");
-        for(int i = 0; i < loadedCards.Length; i++)
-        {
-            loadedCards[i].cardData.icon = Resources.Load<Sprite>("Card/IconTest");
-            Debug.Log(loadedCards[i].cardData.icon);
-        }
-        allCards = loadedCards.ToList();
-    }
-
-    /// <summary>
-    /// 기본적으로 3장의 카드를 무작위로 가져온다.
-    /// 획득 제한 및 카드가 부족한 경우 3장 미만으로 반환할 수 있음.
-    /// </summary>
-    /// <param name="getCount">가져올 카드 개수</param>
-    /// <returns>선택된 카드 리스트</returns>
-    public List<PlayerCardData> GetCards(int getCount = 3)
-    {
-        // 현재 구현되지 않음, null 반환
-        return null;
-    }
 }
